10118112880e9e07cafd6d03271ff56b76a0e6b3
Gary Orsargos <garman9813@gmail.com>
2024-11-03 22:50:06 -0500

    added outline for components, basic file layering and base components created

---
+++ ui/src/components/custom/search/filtersInterface.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+export default class filtersInterface extends Component {
+  render() {
+    return (
+      <div>filtersInterface</div>
+    )
+  }
+}
+
--- ui/src/pages/EditMatrix.tsx
+++ ui/src/pages/EditMatrix.tsx
@@ -1,4 +1,6 @@
 import { Box, Typography } from "@mui/material";
+import FiltersInterface from "../components/custom/search/filtersInterface";
+import MatrixExplorer from "../components/custom/search/matrixExplorer";
 
 export const EditMatrix: React.FC = () => {
   return (
@@ -7,13 +9,18 @@ export const EditMatrix: React.FC = () => {
         This is the edit page.
       </Typography>
       <Box id="searchFiltersBox">
-
+        <FiltersInterface/>
       </Box>
       <Box id="matrixExplorerBox">
-
+        <MatrixExplorer/>
       </Box>
       <Box id="matrixEditorBox">
+        <Box id="userListBox">
 
+        </Box>
+        <Box id="editMatrixBox">
+
+        </Box>
       </Box>
     </>
   );
