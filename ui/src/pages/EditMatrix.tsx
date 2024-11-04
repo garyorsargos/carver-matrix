ffa9be509665ee0d99bbbfa5fa24992b2a184f60
Gary Orsargos <garman9813@gmail.com>
2024-11-03 22:37:15 -0500

    Inital containers

--- ui/src/pages/EditMatrix.tsx
+++ ui/src/pages/EditMatrix.tsx
@@ -1,10 +1,21 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 export const EditMatrix: React.FC = () => {
   return (
-    <Typography data-testid="edit-page" sx={{ color: "#FFF" }}>
-      This is the edit page. Congratulations!
-    </Typography>
+    <>
+      <Typography data-testid="edit-page" sx={{ color: "#FFF" }}>
+        This is the edit page.
+      </Typography>
+      <Box id="searchFiltersBox">
+
+      </Box>
+      <Box id="matrixExplorerBox">
+
+      </Box>
+      <Box id="matrixEditorBox">
+
+      </Box>
+    </>
   );
 };
 
