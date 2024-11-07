5e95c0a231e2dc41752c21ce5f1552a5791fc75e
Gary Orsargos <123672165+garyorsargos@users.noreply.github.com>
2024-11-07 14:32:23 -0500

    CARVER 87, Edit Page Skeleton (#9)
    * Inital containers
    
    * added outline for components, basic file layering and base components created

---
+++ ui/src/components/custom/search/matrixCardLg.tsx
@@ -0,0 +1,11 @@
+import { Card } from '@mui/material'
+import React, { Component } from 'react'
+
+export default class matrixCardLg extends Component {
+  render() {
+    return (
+      <Card>MatrixCard</Card>
+    )
+  }
+}
+
--- ui/src/pages/EditMatrix.tsx
+++ ui/src/pages/EditMatrix.tsx
@@ -1,10 +1,28 @@
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import FiltersInterface from "../components/custom/search/filtersInterface";
+import MatrixExplorer from "../components/custom/search/matrixExplorer";
 
 export const EditMatrix: React.FC = () => {
   return (
-    <Typography data-testid="edit-page" sx={{ color: "#FFF" }}>
-      This is the edit page. Congratulations!
-    </Typography>
+    <>
+      <Typography data-testid="edit-page" sx={{ color: "#FFF" }}>
+        This is the edit page.
+      </Typography>
+      <Box id="searchFiltersBox">
+        <FiltersInterface/>
+      </Box>
+      <Box id="matrixExplorerBox">
+        <MatrixExplorer/>
+      </Box>
+      <Box id="matrixEditorBox">
+        <Box id="userListBox">
+
+        </Box>
+        <Box id="editMatrixBox">
+
+        </Box>
+      </Box>
+    </>
   );
 };
 
