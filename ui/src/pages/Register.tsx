2f3b368d45290814249bb31c80d4de2a295d9a0d
Tran Pham <tranpham9@yahoo.com>
2024-11-10 16:08:47 -0500

    added comments for register page

--- ui/src/pages/Register.tsx
+++ ui/src/pages/Register.tsx
@@ -2,13 +2,23 @@ import React, { useState } from "react";
 import { Box, Button } from "@mui/material";
 import LoginRegisterForm from "../components/LoginRegisterForm";
 
+// TODO: add logic to handle the form submission
+
+// Defines Register as a functional component
 export const Register: React.FC = () => {
+  // State for the login and register mode
   const [mode, setMode] = useState<"login" | "register">("register");
 
+  /**
+   * Switches between login and register mode
+   */
   const handleToggleMode = () => {
     setMode((prevMode) => (prevMode === "login" ? "register" : "login"));
   };
 
+  /**
+   * Handle form submission for login and register modes
+   */
   const handleSubmit = (data: {
     email: string;
     password: string;
